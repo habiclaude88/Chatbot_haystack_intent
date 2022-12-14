61b33884c7a8657daf0d67f1d0aef3a3e3499971
Jean Claude Habimana <habiclaude88@gmail.com>
2022-12-14 10:48:27 +0200

    added google translate api

--- hay.py
+++ hay.py
@@ -66,9 +66,21 @@ def get_answer(q):
 
 
 
-from easynmt import EasyNMT
+import requests
+import json
 
-model = EasyNMT('OPUS-MT')
+
+class translator:
+    api_url = "https://translate.googleapis.com/translate_a/single"
+    client = "?client=gtx&dt=t"
+    dt = "&dt=t"
+
+    #fROM English to Kinyarwanda
+    def translate(text : str , target_lang : str, source_lang : str):
+        sl = f"&sl={source_lang}"
+        tl = f"&tl={target_lang}"
+        r = requests.get(translator.api_url+ translator.client + translator.dt + sl + tl + "&q=" + text)
+        return json.loads(r.text)[0][0][0]
 
 
 
@@ -85,7 +97,7 @@ def answering(qs):
     lang = detect(qs) #DETECT LANGUAGE
 
     # translating the question
-    question = model.translate(raw_query, source_lang=lang, target_lang='en')
+    question = translator.translate(raw_query, source_lang=lang, target_lang='en')
 
     # get the answer
     response = get_answer(question) 
@@ -93,12 +105,12 @@ def answering(qs):
     answer = response['answers'][0].to_dict()['answer']
 
     # translate back to original language
-    answer = model.translate(answer, source_lang='en', target_lang=lang)
+    answer = translator.translate(answer, source_lang='en', target_lang=lang)
     # return answer
     if response['answers'][0].to_dict()['score'] <= 0.6:
       answer = 'No Answer'
 
-      answer = model.translate(answer, source_lang='en', target_lang=lang)
+      answer = translator.translate(answer, source_lang='en', target_lang=lang)
 
 
     return answer
